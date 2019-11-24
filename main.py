b1c60705402f41a76bb307610688ba319f53bf89
Balazs Kertesz <kertesz.balazs92@gmail.com>
2019-11-24 23:49:07 +0100

    Added gui elements

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import stack, memory
+from guizero import App, TextBox, PushButton, Text
 
 stack_cpu = stack.Stack(32)
 cpu_memory = memory.Memory(64)
 
-def setup():
+def do_nothing():
     pass
+
+def setup():
+    app = App(title="Stack Machine")
+    address_label = Text(app, text="Address:")
+    address_value = TextBox(app)
+
+    data_label = Text(app, text="Data:")
+    data_value = TextBox(app)
+
+    load_button = PushButton(app, text="Load", command=do_nothing)
+    save_button = PushButton(app, text="Save", command=do_nothing)
+    run_button = PushButton(app, text="Run", command=do_nothing)
+
+    app.display()
 
 def main():
     pass
 
-
 if __name__ == "__main__":
+    setup()
     main()
